04828014f032fb8857f5dde26ea6c21ac7e08ddf
Lantrix <lantrix@pobox.com>
2015-09-18 12:45:49 +1000

    Added chefdk

--- install-mac.sh
+++ install-mac.sh
@@ -82,7 +82,8 @@ ALL_THE_THINGS_CASK=\
 ' iterm2'\
 ' font-source-code-pro'\
 ' font-source-code-pro-for-powerline'\
-' functionflip'
+' functionflip'\
+' chefdk'
 
 CHROME_CASK_DIR="/opt/homebrew-cask/Caskroom/google-chrome/latest/Google\ Chrome.app"
 #The Mac App Store version of 1Password won't work with a Homebrew-Cask-linked Google Chrome. To bypass this limitation we move Chrome to Applications
