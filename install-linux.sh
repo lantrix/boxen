db08c22f5a572b1a407a40123ad4f505d76eddf0
Lantrix <lantrix@pobox.com>
2015-10-02 14:41:10 +1000

    Add admin user

---
+++ install-linux.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+#Config
+GITHUB_USER="lantrix"
+ADMIN_USER="myadminuser"
+RUBY_VERSION="2.2"
+
+function execute_after_confirm {
+	read -p "$1 ($2) ? [y/n] " -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]
+	then
+		shift
+		for var in "$@"
+		do
+			echo $var
+			eval $var
+			return_code=$?
+			if [[ $return_code != 0 ]] ; then
+				exit $return_code
+			fi
+		done
+	fi
+}
+
+execute_after_confirm \
+	'Update Ubuntu' \
+	'apt-get update' \
+	'apt-get upgrade'
+
+setup_admin_user \
+	'Install Admin User' \
+	'groupadd admin'
+	"useradd -d /home/$adminuser -m $adminuser -g admin" \
+	"passwd $adminuser"
+
+ALL_THE_THINGS_APT=\
+'git'\
+' git-flow'\
+' jq'\
+' gpg'\
+' wget'\
+' node'\
+' vim'\
+' tree'
+
+execute_after_confirm \
+	'Install useful packages' \
+	"apt-get install $ALL_THE_THINGS_APT" \
+
+execute_after_confirm \
+	"Install RVM" \
+	"#curl -sSL https://get.rvm.io | bash -s stable" \
+	"curl -sSL https://rvm.io/mpapis.asc | gpg --import -" \
+	"\\curl -sSL https://get.rvm.io | bash -s stable" \
+	"source $HOME/.rvm/scripts/rvm" \
+	"rvm install $RUBY_VERSION" \
+	"rvm --default use $RUBY_VERSION"
+
+execute_after_confirm \
+	"Install Homesick" \
+	'gem install homesick' \
+	"homesick clone $GITHUB_USER/dotfiles" \
+	"homesick symlink dotfiles" \
+	"homesick clone $GITHUB_USER/dotfiles-vim" \
+	"homesick symlink dotfiles-vim" \
+	'vim +PluginInstall +qall'
