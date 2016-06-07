5fc187bd88a2d1015bfa89ece668e0bbcbfaeec6
Lantrix <lantrix@pobox.com>
2016-06-07 14:42:59 +1000

    Links to git repo got files used by boxstarter

--- install-windows.cmd
+++ install-windows.cmd
@@ -1,3 +1,3 @@
 rem Boxstarter Bootstrap Windows 10
-START http://boxstarter.org/package/nr/url?https://gist.githubusercontent.com/lantrix/84b6f50e621f3eb0dedcf4bcac085361/raw/090dc0b398657dbc951eb3d2ea3158e07a7a21f2/Invoke-Bootstrap.ps1
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/lantrix/boxen/master/boxstarter/Invoke-Bootstrap.ps1
 powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Complete-Bootstrap.ps1"
