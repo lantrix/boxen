d976af45af69611f5efa1cd39704d6bac102a580
Lantrix <lantrix@pobox.com>
2016-06-09 14:46:19 +1000

    Split out post boxstarter steps

--- install-windows.cmd
+++ install-windows.cmd
@@ -1,5 +1,2 @@
 rem Boxstarter Bootstrap Windows 10
 START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/lantrix/boxen/master/boxstarter/Invoke-Bootstrap.ps1
-powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Complete-Bootstrap.ps1"
-powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Configure-SublimeConfig.ps1"
-powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Configure-PowerShellProfile.ps1"
