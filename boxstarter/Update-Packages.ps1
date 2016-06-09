caae510d15798e17ef55faf28b07184420d243a5
Lantrix <lantrix@pobox.com>
2016-06-09 16:04:13 +1000

    Add choco upgrade scripts

---
+++ boxstarter/Update-Packages.ps1
@@ -0,0 +1,42 @@
+try {
+  # Tools
+  cup slack
+  cup dropbox
+  cup 1password
+  cup sublimetext3
+  cup googlechrome
+  cup beyondcompare
+  cup sourcetree
+  cup git -params "/GitAndUnixToolsOnPath /NoAutoCrlf"
+  cup git-credential-manager-for-windows
+  cup githubforwindows
+  cup rdm
+  cup putty
+  cup sourcecodepro
+
+  # Media
+  cup spotify
+
+  # Powershell
+  cup windowsazurepowershell
+  cup AWSTools.Powershell
+  cup poshgit
+
+  # Dev
+  cup visualstudio2015community -packageParameters "--AdminFile https://raw.githubusercontent.com/lantrix/boxen/master/boxstarter/AdminDeployment.xml"
+  cup VisualStudioCode
+  cup sysinternals
+  cup windbg
+  cup dotPeek
+
+  # Basic windows stuff
+  Install-WindowsUpdate -AcceptEula
+  Update-ExecutionPolicy Unrestricted
+  Set-ExplorerOptions -showHidenFilesFoldersDrives -showProtectedOSFiles -showFileExtensions
+  Set-TaskbarSmall
+
+  Write-ChocolateySuccess 'Windows boostrapped'
+} catch {
+  Write-ChocolateyFailure 'Windows Bootstrap: ' $($_.Exception.Message)
+  throw
+}
---
+++ update-windows.cmd
@@ -0,0 +1,2 @@
+rem Boxstarter Bootstrap Windows 10
+START http://boxstarter.org/package/nr/url?https://raw.githubusercontent.com/lantrix/boxen/master/boxstarter/Update-Packages.ps1
