e5e89f974cc6311625a643290fbe77a088541ba8
Lantrix <lantrix@pobox.com>
2016-06-09 14:46:19 +1000

    Split out post boxstarter steps

---
+++ install-post-steps.cmd
@@ -0,0 +1,6 @@
+rem Post Steps for Boxstarter Bootstrap Windows 10
+powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Complete-Bootstrap.ps1"
+pause
+powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Configure-SublimeConfig.ps1"
+pause
+powershell.exe -ExecutionPolicy Bypass -Command ".\boxstarter\Configure-PowerShellProfile.ps1"
