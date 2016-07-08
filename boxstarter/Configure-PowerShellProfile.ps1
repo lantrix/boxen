5922eb4bbacc7c3af35004890b136a60c1de1fd0
Lantrix <lantrix@pobox.com>
2016-07-08 11:46:35 +1000

    Better find of profile

--- boxstarter/Configure-PowerShellProfile.ps1
+++ boxstarter/Configure-PowerShellProfile.ps1
@@ -2,14 +2,15 @@
 Write-Host "Press any key once Dropbox Synced ..."
 $x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
 
+$DocumentsPath = [Environment]::GetFolderPath("MyDocuments")
 $syncedProfile = "$env:HOME\Dropbox\syncdata\WindowsPowerShell\Profile.ps1"
-$iseProfile = "$env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
-$psProfile = "$env:HOME\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+$iseProfile = Join-Path $DocumentsPath -ChildPath "WindowsPowerShell\Microsoft.PowerShellISE_profile.ps1"
+$psProfile = Join-Path $DocumentsPath -ChildPath "WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
 
 if ((test-path $syncedProfile) -eq $true) {
     Write-Host "Setup PowerShell Profile from Dropbox"
-    Remove-Item -Path $iseProfile -Force
-    Remove-Item -Path $psProfile -Force
+    Remove-Item -Path $iseProfile -Force -ErrorAction SilentlyContinue
+    Remove-Item -Path $psProfile -Force -ErrorAction SilentlyContinue
     Copy-Item "..\powershell\Microsoft.PowerShell_profile.ps1" -Destination $psProfile
     Copy-Item "..\powershell\Microsoft.PowerShellISE_profile.ps1" -Destination $iseProfile
 } else {
