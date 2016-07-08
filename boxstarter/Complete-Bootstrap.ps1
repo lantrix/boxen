842a2e3c57123700fe0355423fa36428065c84e7
Lantrix <lantrix@pobox.com>
2016-07-08 12:47:02 +1000

    Updated for .NET core 1.0

--- boxstarter/Complete-Bootstrap.ps1
+++ boxstarter/Complete-Bootstrap.ps1
@@ -2,10 +2,10 @@ Set-ExecutionPolicy -Force RemoteSigned
 Set-PSRepository -Name 'PSGallery' -InstallationPolicy Trusted
 iwr https://chocolatey.org/install.ps1 | iex #Choco Install
 
-#Azure VS2015 SDK - https://msdn.microsoft.com/library/mt695655.aspx
+#.NET CORE 1.0 - https://www.microsoft.com/net/core#windows
+Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?LinkId=817245 -OutFile $env:TEMP\DotNetCore.1.0.0-VS2015Tools.Preview2.exe
+& $env:TEMP\DotNetCore.1.0.0-VS2015Tools.Preview2.exe /passive
+
+#Azure Azure SDK 2.9 for .NET (VS2015) - https://azure.microsoft.com/en-us/blog/announcing-visual-studio-azure-tools-and-sdk-2-9/
 Invoke-WebRequest -Uri http://go.microsoft.com/fwlink/?LinkId=746481 -OutFile $env:TEMP\VWDOrVs2015AzurePack.exe
 & $env:TEMP\VWDOrVs2015AzurePack.exe
-
-#.NET CORE RC2 - https://www.microsoft.com/net/core#windows
-Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?LinkId=798481 -OutFile $env:TEMP\DotNetCore.1.0.0.RC2-VS2015Tools.Preview1.exe
-& $env:TEMP\DotNetCore.1.0.0.RC2-VS2015Tools.Preview1.exe /passive
