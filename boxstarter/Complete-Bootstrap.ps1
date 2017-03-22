7c5f81c2e6a598656de9a66aecfe731e8648f84c
Lantrix <lantrix@pobox.com>
2017-03-22 15:49:21 +1100

    Version bumps

--- boxstarter/Complete-Bootstrap.ps1
+++ boxstarter/Complete-Bootstrap.ps1
@@ -1,10 +1,6 @@
 Set-ExecutionPolicy -Force RemoteSigned
 iwr https://chocolatey.org/install.ps1 | iex #Choco Install
 
-#.NET CORE 1.0.1 - https://www.microsoft.com/net/core#windowsvs2015
-Invoke-WebRequest -Uri https://go.microsoft.com/fwlink/?LinkID=827546 -OutFile $env:TEMP\DotNetCore.1.0.0-VS2015Tools.Preview2.exe
-& $env:TEMP\DotNetCore.1.0.0-VS2015Tools.Preview2.exe /passive
-
-#Azure Azure SDK 2.9 for .NET (VS2015) - https://azure.microsoft.com/en-us/blog/announcing-visual-studio-azure-tools-and-sdk-2-9/
-Invoke-WebRequest -Uri http://go.microsoft.com/fwlink/?LinkId=746481 -OutFile $env:TEMP\VWDOrVs2015AzurePack.exe
-& $env:TEMP\VWDOrVs2015AzurePack.exe
+#Azure Azure SDK 2.9.6 for .NET (VS2015) - https://www.microsoft.com/en-us/download/details.aspx?id=54289
+Invoke-WebRequest -Uri https://aka.ms/azuresdk2015_296 -OutFile $env:TEMP\VWDOrVs2015AzurePack.2E2.2E9.exe
+& $env:TEMP\VWDOrVs2015AzurePack.2E2.2E9.exe
